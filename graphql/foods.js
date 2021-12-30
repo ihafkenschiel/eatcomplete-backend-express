d73f6200086741ca06f2c9be7b49c9cdf8b44a8e
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-12-30 02:25:23 -0800

    Pagination for foods working

--- graphql/foods.js
+++ graphql/foods.js
@@ -1,9 +1,10 @@
 import { gql } from 'apollo-server-express'
+// Local
 import db from '../sequelize'
 
 export const typeDefs = gql`
   extend type Query {
-    foods: [Food]
+    foods(take: Int, skip: Int): [Food]
     food(id: ID!): Food
   }
   type Food {
@@ -16,7 +17,13 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Query: {
-    foods: async () => db.foods.findAll(),
-    food: async (obj, args) => db.foods.findByPk(args.id),
+    foods: async (_, args) => {
+      const result = await db.foods.findAndCountAll({
+        limit: args.take,
+        offset: args.skip,
+      })
+      return result.rows
+    },
+    food: async (_, args) => db.foods.findByPk(args.id),
   },
 }
